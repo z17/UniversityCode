625016a493d44ba595a77386e5e5dda9e1faace8
Maksim <admin@blweb.ru>
2014-01-16 23:15:37 +0400

    II course work update 2

--- Технологии искуственного интеллекта/курсовик/code/code/code.cpp
+++ Технологии искуственного интеллекта/курсовик/code/code/code.cpp
@@ -11,22 +11,21 @@ using namespace std;
 
 /*
 to do:
-- проверка добавлено ли такое св-во уже (убрать дублирование при описании)
-- для свойств ввести отрицание !(свойство), его приоритет
++ проверка добавлено ли такое св-во уже (убрать дублирование при описании)
++ для свойств ввести отрицание !<свойство>, его приоритет
 - проверка на существование родителя
 - если родителя не существует - создавать объект с пустыми свойствами, если дальше он описывается - дописывать св-ва в этот объект
 - вывод всех родителей
+- вынесение в заголовочный файл части
 */
 
 template <typename T>
-vector<T> merge(vector<T> one, vector<T> two)
+vector<T> merge(vector<T>& one, vector<T>& two)
 {
 	vector<T> res;
 
-	for (int i = 0; i < one.size(); i++)
-	{
-		res.push_back(one[i]);
-	}	
+	res = one;
+
 	for (int i = 0; i < two.size(); i++)
 	{
 		res.push_back(two[i]);
@@ -65,18 +64,48 @@ public:
 	}
 
 	// рекурсивно получаем все свойства класса (свои и всех родителей)
-	vector<string> getAllPropetries()
+	vector<string> getAllProperties()
 	{
-		vector<string> parentsPropetries;
-		if (this->parent != NULL)
+		vector<string> allProperties;
+		if (this != NULL)
 		{
-			parentsPropetries = merge(this->properties,this->parent->getAllPropetries());
+			if (this->parent != NULL)
+			{
+				allProperties = merge(this->properties,this->parent->getAllProperties());
+			}
+			else
+			{
+				allProperties = this->properties;
+			}
 		}
-		else
+		
+		// удаление из выводимого списка отрицаемых свойств
+		int i = 0;
+		while (i < allProperties.size())
 		{
-			parentsPropetries = this->properties;
+			if (allProperties[i][0] == '!')
+			{
+				string invProperty = allProperties[i].substr(1,allProperties[i].size()-1);
+				int nPos = this->findProperty(invProperty,allProperties);
+
+				if (nPos >= 0 && nPos > i)
+				{
+					// удаление !<свойство> и <свойство>
+					allProperties.erase(allProperties.begin()+nPos);
+					allProperties.erase(allProperties.begin()+i);
+					i--;
+					i--;
+				}
+				if  (nPos < i)
+				{
+					// удаление !<свойство> если его позиция дальше позиции <свойство>
+					allProperties.erase(allProperties.begin()+i);
+					i--;
+				}
+			}
+			i++;
 		}
-		return parentsPropetries;
+		return allProperties;
 	}
 
 	// вывод класса на экран
@@ -85,23 +114,24 @@ public:
 		cout << "Class" << endl;
 		cout << "Name: " << this -> name << endl;
 		cout << "Properties: ";
-		vector<string> currentPropetries;
-		currentPropetries = this -> getAllPropetries();			
+		vector<string> currentProperties;
+		currentProperties = this -> getAllProperties();			
 
-		for (int i = 0; i < currentPropetries.size(); i++)
+		for (int i = 0; i < currentProperties.size(); i++)
 		{
-			cout << currentPropetries[i];
-			if (i + 1 != currentPropetries.size())
+			cout << currentProperties[i];
+			if (i + 1 != currentProperties.size())
 			{
 				cout << ", ";
 			}
 		}
 		cout << endl << "---------" << endl;
 	}
+
 	bool findProperty(string nameProperty)
 	{
 		bool fl = false;
-		vector<string> allPropertes = this->getAllPropetries();
+		vector<string> allPropertes = this->getAllProperties();
 		
 		int i = -1;		
 		do
@@ -115,6 +145,23 @@ public:
 			fl = true;
 		}
 		return fl;
+	}
+
+	// возвращает номер nameProperty в массиве list или -1, если не найден
+	int findProperty (string nameProperty, vector<string> list)
+	{
+		int i = -1;		
+		do
+		{
+			i++;	
+		}
+		while (list[i] != nameProperty && i+1 < list.size());
+	
+		if (list[i] != nameProperty)
+		{
+			i = -1;
+		}
+		return i;
 	}
 };
 
@@ -150,6 +197,32 @@ struct classWrap
 			{
 				parent = this->massOfClasses[i];
 			}
+		}
+
+		// проверка дублирования свойств. Если есть дублирование - удаляем
+		vector<string> test = merge(properties, parent->getAllProperties());
+		int lenght = properties.size();
+		int i = 0;
+		while (i < lenght)
+		{
+			int k = 0;
+			for (int j = 0; j < test.size(); j++)
+			{
+				if (test[i] == test[j])
+				{
+					k++;
+				}
+			}
+			if (k > 1)
+			{
+				test.erase(test.begin() + i);
+				lenght--;
+			}
+			else
+			{
+				i++;
+			}
+			properties.assign(test.begin(), test.begin()+lenght);
 		}
 
 		customClass *newClass = new customClass(name, parent, properties);
@@ -213,15 +286,19 @@ struct classWrap
 	bool isPropety(string className, string propetyName)
 	{
 		bool fl = false;
-		int i = -1;
-		do
+		if (this -> massOfClasses.size() != 0 )
 		{
-			i++;	
-		}
-		while (this->massOfClasses[i]->getNameClass() != className && i+1 < this->massOfClasses.size());
-		if (this->massOfClasses[i]->getNameClass() == className)
-		{
-			fl = this->massOfClasses[i]->findProperty(propetyName);
+			int i = -1;
+			do
+			{
+				i++;	
+			}
+			while (this->massOfClasses[i]->getNameClass() != className && i+1 < this->massOfClasses.size());
+
+			if (this->massOfClasses[i]->getNameClass() == className)
+			{
+				fl = this->massOfClasses[i]->findProperty(propetyName);
+			}
 		}
 		return fl;
 	}
